beeb72d2cc7992ab620a95938c6a896d82c14811
Juliano Ferreira Rodrigues <julianof.rodrigues@hotmail.com>
2021-01-26 16:10:14 -0300

    Imersão Aula 02

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizLogo from '../src/components/QuizLogo';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -24,16 +28,41 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Quiz em Dev</title>
+      </Head>
       <QuizContainer>
-        
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>{db.description}</p>
+          <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  console.log(infosDoEvento.target.value);
+                  // State
+                  // name = infosDoEvento.target.value;
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Qual seu nome?"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+              </button><br></br>
+              <label>{name}</label>
+            </form>
           </Widget.Content>
         </Widget>
 
---
+++ pages/quiz.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Quiz() {
+  return (
+    <div>
+      <Head>
+        <title>Quiz em Dev</title>
+      </Head>
+    </div>
+  );
+}
